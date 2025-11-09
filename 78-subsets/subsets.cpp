0d1ad7223e73fc25ba0fef86749ecce1d2400853
Mohamed Hedi Aissi <148924541+hediske@users.noreply.github.com>
2025-11-09 02:44:45 +0100

    Time: 2 ms (39.35%) | Memory: 10 MB (41.51%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> vct;
-        for(int i = 0 ; i<pow(2,nums.size()) ; i++ ){
-            bitset<10> bt(i);
-            vector<int> v;
-            for (int i = 0 ; i<nums.size() ; i++){
-                if(bt[i] == 1){
-                    v.push_back(nums[i]);
+        int n = nums.size();
+        vector<vector<int>> res ;
+        for (int i = 0 ; i< pow(2,n) ; i++){
+            bitset<11> bt(i);
+            vector<int> vect ;
+            for (int i = 0 ; i< n ; i++){
+                if(bt[i]){
+                    vect.push_back(nums[i]);
                 }
             }
-            vct.push_back(v);
+            res.push_back(vect);
         }
-        return vct;
+        return res;
     }
 };
