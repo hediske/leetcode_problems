2acf59ec9690b3c2dc12417562616e685bf510af
Mohamed Hedi Aissi <148924541+hediske@users.noreply.github.com>
2025-11-11 00:21:44 +0100

    Time: 0 ms (100.00%) | Memory: 9.3 MB (43.73%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector <vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for (int i = 0 ; i<m;i++){
+            for (int j = 0 ;j<n;j++ ){
+                if(j<n-1){
+                    dp[i][j+1] +=dp[i][j];
+                }
+                if(i<m-1){
+                    dp[i+1][j]+=dp[i][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
