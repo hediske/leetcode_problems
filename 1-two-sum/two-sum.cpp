739511a83da3d0f4dfcfaacf5196e30a5daf30fd
Mohamed Hedi Aissi <148924541+hediske@users.noreply.github.com>
2026-08-22 17:16:11 +0100

    Time: 3 ms (66.66%) | Memory: 15 MB (25.57%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        unordered_map<int,vector<int>> pos;
-
-        for (int i = 0;i<nums.size();++i){
-            if (pos.find(nums[i]) == pos.end()){
-                vector<int> st;
-                st.push_back(i);
-                pos[nums[i]] = st;
-            }else{
-                pos[nums[i]].push_back(i);
-            }
-        }
-
         vector<int> res;
-        for (const auto& p : pos){
-            if (pos.find(target - p.first) != pos.end()){
-                if((target - p.first) == p.first){
-                    if(p.second.size()>1){
-                        res.push_back(p.second[0]);
-                        res.push_back(p.second[1]);
-                        break;
-                    }
-                }else{
-                    res.push_back(p.second[0]);
-                    res.push_back(pos[target-p.first][0]);
-                    break;
-                }
+        unordered_map<int,int> pos;
+        for (int i =0; i<nums.size();++i ){
+            if(pos.find(target - nums[i]) != pos.end()){
+                res.push_back(i);
+                res.push_back(pos[target-nums[i]]);
+                break;
             }
+            pos[nums[i]] = i;
         }
         return res;
     }
+    
+
 };
